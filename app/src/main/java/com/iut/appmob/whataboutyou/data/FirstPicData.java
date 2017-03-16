3c16a86158485f582707edd812bb0a616801c0ad
dominique <guydombess@gmail.com>
2017-03-16 09:22:10 +0100

    first pic finish

--- app/src/main/java/com/iut/appmob/whataboutyou/data/FirstPicData.java
+++ app/src/main/java/com/iut/appmob/whataboutyou/data/FirstPicData.java
@@ -4,7 +4,7 @@ import android.support.v7.widget.AppCompatImageButton;
 import android.support.v7.widget.AppCompatImageView;
 import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
@@ -14,6 +14,7 @@ import com.iut.appmob.whataboutyou.R;
 import com.iut.appmob.whataboutyou.User;
 import com.squareup.picasso.Picasso;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -25,7 +26,7 @@ public class FirstPicData implements Data , View.OnClickListener{
 
     private boolean isFinished = false, isStarted = false;
     private AppCompatImageButton refresh;
-    private AppCompatImageView firstPic;
+    private AppCompatImageView firstPic, lastPic;
     private LinearLayout layout;
 
     @Override
@@ -49,18 +50,26 @@ public class FirstPicData implements Data , View.OnClickListener{
                 new GraphRequest.Callback() {
                     public void onCompleted(GraphResponse response) {
                         JSONObject jo = response.getJSONObject();
-                        try {
-                            layout.setVisibility(View.VISIBLE);
-                            JSONObject joPic = jo.getJSONArray("data").getJSONObject(0).getJSONObject("photos").getJSONArray("data").getJSONObject(0).getJSONArray("images").getJSONObject(0);
-                            Picasso.with(layout.getContext()).load(joPic.getString("source")).placeholder(R.drawable.loading_creation_room_black).into(firstPic);
-                            isFinished = true;
-                        } catch (JSONException e) {
-                            e.printStackTrace();
+                        if (jo != null) {
+                            try {
+                                JSONArray ja = jo.getJSONArray("data").getJSONObject(0).getJSONObject("photos").getJSONArray("data");
+                                JSONObject joFirstPic = ja.getJSONObject(0).getJSONArray("images").getJSONObject(0), joLastPic = ja.getJSONObject(ja.length() - 1).getJSONArray("images").getJSONObject(0);
+                                Picasso.with(layout.getContext()).load(joFirstPic.getString("source")).fit().placeholder(R.drawable.loading_creation_room_black).into(firstPic);
+                                Picasso.with(layout.getContext()).load(joLastPic.getString("source")).fit().placeholder(R.drawable.loading_creation_room_black).into(lastPic);
+                                //layout.getChildAt(0).setVisibility(View.VISIBLE);
+                                isFinished = true;
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                                layout.setVisibility(View.GONE);
+                                refresh.setVisibility(View.VISIBLE);
+                                isStarted = false;
+                            }
+                        } else {
                             layout.setVisibility(View.GONE);
                             refresh.setVisibility(View.VISIBLE);
                             isStarted = false;
+                            Toast.makeText(layout.getContext(), "Il n'y a pas internet", Toast.LENGTH_LONG).show();
                         }
-
                     }
                 }
         ).executeAsync();
@@ -70,7 +79,8 @@ public class FirstPicData implements Data , View.OnClickListener{
     public void bind(View v) {
         layout = (LinearLayout) v.findViewById(R.id.firstPicLayout);
         firstPic = (AppCompatImageView) v.findViewById(R.id.firstPicImgView);
-        refresh = (AppCompatImageButton) v.findViewById(R.id.refreshBtnFirstPic) ;
+        lastPic = (AppCompatImageView) v.findViewById(R.id.lastPicImgView);
+        refresh = (AppCompatImageButton) v.findViewById(R.id.refreshBtnFirstPic);
         refresh.setOnClickListener(this);
     }
 
@@ -80,6 +90,7 @@ public class FirstPicData implements Data , View.OnClickListener{
             case R.id.refreshBtnFirstPic:
                 if (!isFinished() && !isStarted()) {
                     refresh.setVisibility(View.GONE);
+                    layout.setVisibility(View.VISIBLE);
                     finish();
                 }
                 break;
